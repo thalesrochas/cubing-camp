03b47e720ccf361f18a8e61511c77e362c38bb43
Thales Rocha <thales_rochasousa@hotmail.com>
2020-04-26 20:18:39 -0300

    Adição da página inicial e algumas páginas secundárias

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
 const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
