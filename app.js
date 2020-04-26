b0d2aee1fb0d2c6931e5a438a0e15c8214fa46b5
João Marcos <joaomarcos25@hotmail.com>
2020-04-26 14:15:12 -0300

    Página principal e componentes adicionados

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log("Servidor rodando na porta:", PORT);
+});
